bff2b55fd0e1af71c5a23d3d556384dd6d237474
Brian Antunes <brian.antunes@yahoo.com>
2022-11-01 21:34:25 -0400

    Added Everything

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import Intro from './Intro'
-import Timeline from './Timeline'
-import './index.css'
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Intro />
     <App />
-    <Timeline />
   </React.StrictMode>
-)
+);
